595b43111636a47ee50335a51509c1ba8e84b72b
Maor <maorazoulay@gmail.com>
2022-04-11 12:26:37 -0400

    Added tests for new deposit eteher function in the contract and updated helper class

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,3 +1,6 @@
+const { default: Web3 } = require('web3');
+const { ETHER_ADDRESS, ether } = require('./helpers');
+
 /* eslint-disable no-undef */
 var tokens = require('./helpers').tokens;
 var EVM_REVERT = require('./helpers').EVM_REVERT;
@@ -6,34 +9,66 @@ const Token = artifacts.require('./Token')
 const Exchange = artifacts.require('./Exchange')
 
 require('chai')
-.use(require('chai-as-promised'))
-.should()
+    .use(require('chai-as-promised'))
+    .should()
 
 contract('Exchange', ([deployer, feeAccount, user1]) => {
-	let exchange
-	let token
+    let exchange
+    let token
     const feePercent = 10
 
-	beforeEach(async () => {
+    beforeEach(async () => {
         // Deploy token
         token = await Token.new()
         // Transfer some tokens to  user1
-        token.transfer(user1, tokens(100), {from: deployer})
+        token.transfer(user1, tokens(100), { from: deployer })
         // Deploy exchange
-		exchange = await Exchange.new(feeAccount, feePercent)
-	})
+        exchange = await Exchange.new(feeAccount, feePercent)
+    })
 
-	describe('deployment', () => {
-		it('tracks the fee account', async () => {
-			const result = await exchange.feeAccount()
-			result.should.equal(feeAccount)
-		})
+    describe('deployment', () => {
+        it('tracks the fee account', async () => {
+            const result = await exchange.feeAccount()
+            result.should.equal(feeAccount)
+        })
 
         it('tracks the fee percent', async () => {
-			const result = await exchange.feePercent()
-			result.toString().should.equal(feePercent.toString())
-		})
-	})
+            const result = await exchange.feePercent()
+            result.toString().should.equal(feePercent.toString())
+        })
+    })
+
+    describe('fallback', () => {
+        it('reverts when ether is sent', async () => {
+            await exchange.sendTransaction({ value: 1, from: user1 }).should.be.rejectedWith(EVM_REVERT)
+        })
+    })
+
+    describe('depositing ether', async () => {
+        let result
+        let amount
+
+        beforeEach(async () => {
+            amount = ether(1);
+            result = await exchange.depositEther({ from: user1, value: amount })
+        })
+
+        it('tracks the ether deposit', async () => {
+            const balance = await exchange.tokens(ETHER_ADDRESS, user1)
+            balance.toString().should.equal(amount.toString())
+        })
+
+
+        it('emits a Deposit event', async () => {
+            const log = result.logs[0]
+            log.event.should.eq('Deposit')
+            const event = log.args
+            event.token.should.equal(ETHER_ADDRESS, 'token address is correct')
+            event.user.should.equal(user1, 'user address is correct')
+            event.amount.toString().should.equal(amount.toString(), 'amount is correct')
+            event.balance.toString().should.equal(amount.toString(), 'balance is correct')
+        })
+    })
 
     describe('depositing tokens', () => {
         let result
@@ -43,8 +78,8 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 
             beforeEach(async () => {
                 amount = tokens(10)
-                await token.approve(exchange.address, amount, {from: user1})
-                result = await exchange.depositToken(token.address, tokens(10), {from: user1})
+                await token.approve(exchange.address, amount, { from: user1 })
+                result = await exchange.depositToken(token.address, tokens(10), { from: user1 })
             })
 
             it('tracks the token deposit', async () => {
@@ -58,26 +93,26 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
             })
 
             it('emits a Deposit event', async () => {
-				const log = result.logs[0]
-				log.event.should.eq('Deposit')
-				const event = log.args
-				event.token.should.equal(token.address, 'token address is correct') 
-				event.user.should.equal(user1, 'user address is correct') 
-				event.amount.toString().should.equal(tokens(10).toString(), 'amount is correct') 
-				event.balance.toString().should.equal(tokens(10).toString(), 'balance is correct') 
-			})
+                const log = result.logs[0]
+                log.event.should.eq('Deposit')
+                const event = log.args
+                event.token.should.equal(token.address, 'token address is correct')
+                event.user.should.equal(user1, 'user address is correct')
+                event.amount.toString().should.equal(amount.toString(), 'amount is correct')
+                event.balance.toString().should.equal(amount.toString(), 'balance is correct')
+            })
         })
 
         describe('failure', () => {
 
-            it('rejects Ether deposits', async() => {
-                
+            it('rejects Ether deposits', async () => {
+                await exchange.depositToken(ETHER_ADDRESS, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
             })
 
             it('fails when no tokens are approved', async () => {
                 // Don't approve any tokens before depositing
-                await exchange.depositToken(token.address, tokens(10), {from: user1}).should.be.rejectedWith(EVM_REVERT)
+                await exchange.depositToken(token.address, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
             })
         })
-	})
+    })
 })
--- test/helpers.js
+++ test/helpers.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-undef */
-const tokens = (n) => {
+const ether = (n) => {
 	return new web3.utils.BN(
 		web3.utils.toWei(n.toString(), 'ether')
-		)
+	)
 }
 
+const tokens = (n) => ether(n)
+
 const EVM_REVERT = 'VM Exception while processing transaction: revert'
+const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 module.exports = {
-	tokens,EVM_REVERT
+	ether, tokens, EVM_REVERT, ETHER_ADDRESS
 }
