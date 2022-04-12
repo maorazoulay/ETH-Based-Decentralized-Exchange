e18e4e28fd1445cf4a6c65038a67f26637c2070b
Maor <maorazoulay@gmail.com>
2022-04-11 22:17:11 -0400

    Formatted exchange contract, added cancel and make order with their events

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -9,8 +9,8 @@ import "./Token.sol";
 // [X] Deposit tokens
 // [X] Withdraw tokens
 // [X] Check balances
-// [] Make order
-// [] Cancel order
+// [X] Make order
+// [X] Cancel order
 // [] Fill order
 // [] Charge fees
 
@@ -22,15 +22,32 @@ contract Exchange {
     // The tokens' mapping -> Key: token_address, value: user_balance of token
     address constant ETHER = address(0); //Store Ether in tokens mapping with blank address
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     // Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
-    event Withdraw(
-        address token,
-        address user,
-        uint256 amount,
-        uint256 balance
-    );
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(uint256 id, address user, address tokenGet, uint256 amountGet, 
+                address tokenGive, uint256 amountGive, uint256 timestamp);
+    event Cancel(uint256 id, address user, address tokenGet, uint256 amountGet, 
+                address tokenGive, uint256 amountGive, uint256 timestamp);
+
+    // Structs
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
+
+    // a way to model the order
+    // a way to store the order
+    // Add the order to storage
 
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
@@ -71,11 +88,24 @@ contract Exchange {
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
-    function balanceOf(address _token, address _user)
-        public
-        view
-        returns (uint256)
-    {
+    function balanceOf(address _token, address _user) public view returns (uint256){
         return tokens[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet,uint256 _amountGet,address _tokenGive,uint256 _amountGive) public {
+        orderCount = orderCount.add(1);
+        uint256 _id = orderCount;
+        orders[_id] = _Order(_id,msg.sender,_tokenGet,_amountGet,_tokenGive,_amountGive,now);
+        emit Order(_id,msg.sender,_tokenGet,_amountGet,_tokenGive,_amountGive,now); 
+    }
+
+    function cancelOrder(uint256 _id)public {
+        // Must be "my" order
+        // Order must exist!
+        _Order storage _order = orders[_id];
+        require(address(_order.user) == msg.sender);
+        require(_order.id == _id);
+        orderCancelled[_id] = true; 
+        emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, _order.timestamp);
     }
 }
