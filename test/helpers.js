058babdc5806847966ae41f581aa5cebfb8eb9df
Maor <maorazoulay@gmail.com>
2022-04-14 23:05:55 -0400

    Added wait function to helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-undef */
+const wait = (seconds) => {
+	const milliseconds = seconds * 1000
+	return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+
 const ether = (n) => {
 	return new web3.utils.BN(
 		web3.utils.toWei(n.toString(), 'ether')
@@ -11,5 +16,5 @@ const EVM_REVERT = 'VM Exception while processing transaction: revert'
 const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 module.exports = {
-	ether, tokens, EVM_REVERT, ETHER_ADDRESS
+	ether, tokens, EVM_REVERT, ETHER_ADDRESS, wait
 }
