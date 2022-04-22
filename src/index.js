176f9fc31cfc255ab3c2307ffb901e31640fc334
Maor <maorazoulay@gmail.com>
2022-04-21 20:51:36 -0400

    Now loading web3, accounts, networkId, token and exchange contracts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import'bootstrap/dist/css/bootstrap.css';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import configureStore from './store/configureStore'
+import 'bootstrap/dist/css/bootstrap.css'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={configureStore()}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
