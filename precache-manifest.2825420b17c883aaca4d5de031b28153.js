927e50c54d327436d5d572b9a1692647a432af0d
maorazoulay <maorazoulay@gmail.com>
2022-05-10 16:02:31 -0400

    Updates

---
+++ precache-manifest.2825420b17c883aaca4d5de031b28153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f65f63d191de4fd4f9ea5a25ec1441e",
+    "url": "/ETH-Based-Decentralized-Exchange/index.html"
+  },
+  {
+    "revision": "2e9d2817cb951504ef2b",
+    "url": "/ETH-Based-Decentralized-Exchange/static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "e81b5fa3c45d36124c8a",
+    "url": "/ETH-Based-Decentralized-Exchange/static/css/main.b7205f96.chunk.css"
+  },
+  {
+    "revision": "2e9d2817cb951504ef2b",
+    "url": "/ETH-Based-Decentralized-Exchange/static/js/2.53b809db.chunk.js"
+  },
+  {
+    "revision": "51449207757da960bf9e6a29f39bf89d",
+    "url": "/ETH-Based-Decentralized-Exchange/static/js/2.53b809db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81b5fa3c45d36124c8a",
+    "url": "/ETH-Based-Decentralized-Exchange/static/js/main.5d7c3080.chunk.js"
+  },
+  {
+    "revision": "1be9c12f312df44cf5da",
+    "url": "/ETH-Based-Decentralized-Exchange/static/js/runtime-main.c706b283.js"
+  }
+]);
