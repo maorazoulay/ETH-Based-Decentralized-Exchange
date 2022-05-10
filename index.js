5b56bdd8ddb0e7ff658223275056390c00aeb13d
maorazoulay <maorazoulay@gmail.com>
2022-05-10 16:12:16 -0400

    moved index to root

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css'
-import App from './components/App'
-import configureStore from './store/configureStore'
-import * as serviceWorker from './serviceWorker'
+import App from './src/components/App'
+import configureStore from './src/store/configureStore'
+import * as serviceWorker from './src/serviceWorker'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
