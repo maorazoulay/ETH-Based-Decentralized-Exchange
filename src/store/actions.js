204438c53fe95b0b1d76820ec687ec7251b941f1
Maor <maorazoulay@gmail.com>
2022-05-09 21:21:54 -0400

    Added cancel orders functionality - reflected in actions, interactions, selectors and reducers

--- src/store/actions.js
+++ src/store/actions.js
@@ -49,3 +49,18 @@ export function web3Loaded(connection) {
       allOrders
     }
   }
+  
+  // Cancel Order
+  export function orderCancelling() {
+    return {
+      type: 'ORDER_CANCELLING'
+    }
+  }
+  
+  export function orderCancelled(order) {
+    return {
+      type: 'ORDER_CANCELLED',
+      order
+    }
+  }
+  
